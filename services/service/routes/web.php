039a3d392c198a27884425c0b16899d0b5f043f3
kr95656 <katmsg8239@gmail.com>
2021-08-15 01:36:45 +0900

    route 設定

--- services/service/routes/web.php
+++ services/service/routes/web.php
@@ -13,10 +13,44 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::redirect('/top', 'groups.top');
+Route::redirect('/', 'dashboard');
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+
+Route::middleware(['auth:sanctum'])->group(function () {
+
+    // ホーム画面
+    Route::get('/dashboard', function() {
+        return view('dashboard');
+    })->name('dashboard');
+
+    // グループ詳細画面
+    Route::get('/groups/{id}', function() {
+        return 'groups show';
+    })->whereNumber('id');
+
+    // グループ作成画面
+    Route::get('/groups/create', function() {
+        return 'groups create';
+    });
+
+    // グループ作成
+    Route::post('/groups', function() {
+        return redirect('dashboard');
+    });
+
+    // グループ編集画面
+    Route::get('/groups/{id}/edit', function () {
+        return 'groups edit';
+    })->whereNumber('id');
+
+    // グループ編集
+    Route::put('/groups/{id}', function() {
+        return redirect('dashboard');
+    });
+
+    // グループ削除
+    Route::delete('/groups/{id}', function() {
+        return redirect('dashboard');
+    });
+});
